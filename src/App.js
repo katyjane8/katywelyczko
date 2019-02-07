203a3e8b997d1b33f01398b0547fb57ab42aab35
katy <katy.jane8@gmail.com>
2019-02-07 15:43:01 -0700

    test app.js

--- src/App.js
+++ src/App.js
@@ -7,18 +7,13 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <ul>
+            <li>home</li>
+            <li>social</li>
+            <li>resume</li>
+          </ul>
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
         </header>
       </div>
     );
