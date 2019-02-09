3a3d6096e418cf8d255b095402603eacca224943
Kathrine Welyczko <kathrine@KatBook-Pro.local>
2019-02-09 14:33:00 -0700

    fun styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './animation.js'
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="nav">
-          <ul>
-            <a href="/">home</a>
-          </ul>
-          <ul>
-            <a href="/resume">resume</a>
-          </ul>
-          <ul>
-            <a href="/social">social</a>
-          </ul>
+          <div className='btn'>
+            <ul>
+              <a href="/">home</a>
+            </ul>
+          </div>
+            <ul>
+              <a href="/resume">resume</a>
+            </ul>
+            <ul>
+              <a href="/social">social</a>
+            </ul>
         </header>
-        <h1>katy welyczko dot com</h1>
-        <img src={logo} className="App-logo" alt="logo" />
+          <div className="type-js headline">
+            <h1 className='text-js'>katy welyczko dot com</h1>
+          </div>
       </div>
     );
   }
