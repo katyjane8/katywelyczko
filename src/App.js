3a5fb88a3b6fba65932e4d6d3486768007ef3c1c
Kathrine Welyczko <kathrine@KatBook-Pro.local>
2019-02-09 15:44:02 -0700

    react router added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Social from './Social.js'
-import Resume from './Resume.js'
-import Home from './Home.js'
-import './animation.js'
-import './App.scss';
+import Social from 'Social.js'
+import Resume from 'Resume.js'
+import Home from 'Home.js'
+import 'animation.js'
+import 'App.scss';
 
 class App extends Component {
   render() {
