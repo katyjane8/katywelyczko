d714af5b208274eaaf2ba3a2acb588520834b04b
katy <katy.jane8@gmail.com>
2019-02-11 16:04:55 -0700

    ignore modules

---
+++ build/precache-manifest.076ae9388714e7850faa77299d4b2381.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ee84d7b2c82b0b9015d5",
+    "url": "/static/js/main.ee84d7b2.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "ee84d7b2c82b0b9015d5",
+    "url": "/static/css/main.35b8d76f.chunk.css"
+  },
+  {
+    "revision": "11143de1e43f554b89d1a613866087a4",
+    "url": "/index.html"
+  }
+];
