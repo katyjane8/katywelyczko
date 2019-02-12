af028e45814277e6fc942d99a6e0c0cb9542551a
Kathrine Welyczko <kathrine@KatBook-Pro.local>
2019-02-11 19:35:52 -0700

    troubleshooting heroku

---
+++ server.js
@@ -0,0 +1,6 @@
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join('build', 'index.html'));
+    });
+}
