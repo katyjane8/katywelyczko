4c3c36629878dbdf413b9f2b0a4885ce44e591d2
Kathrine Welyczko <kathrine@KatBook-Pro.local>
2019-02-11 19:59:43 -0700

    switch to firebase hosting

--- server.js
+++
@@ -1,6 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join('build', 'index.html'));
-    });
-}
